17a58e4640345d5ebef3adeeee3fc3e0f0505512
Amahle <amahlez798@gmail.com>
2025-05-10 08:08:39 +0200

    Auth issues are getting fixed, just need to make a login page

--- src/app/revenues/page.jsx
+++ src/app/revenues/page.jsx
@@ -2,9 +2,22 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function RevenuesPage() {
   const supabase = await createClient()
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser()
+
   const { data: revenue, error } = await supabase
     .from('revenue')
     .select('*')
+    .eq('user_id', user?.id)
+    
+
+  if (userError) {
+    return <p>Error fetching user: {userError.message}</p>
+  }
+
   if (error) {
     return <p>Error: {error.message}</p>
   }
@@ -12,6 +25,7 @@ export default async function RevenuesPage() {
   return (
     <div>
       <h1>Revenues</h1>
+      <h2>Logged in as: {user?.email || user?.id}</h2>
       <pre>{JSON.stringify(revenue, null, 2)}</pre>
     </div>
   )
